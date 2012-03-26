16b9ff4132816e175c91d3bb8197cba7d0e0c115
Vincent Rabaud <vrabaud@willowgarage.com>
2012-03-26 09:32:22 -0700

    forgot the setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Tabletop',
+      version='1.0.0',
+      description='Tabletop object recognition',
+      packages=['tabletop'],
+      package_dir={'':'python'}
+)
