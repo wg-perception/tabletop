312efb2feb99f9fbcd0244ffb9bacc538c3f4386
Vincent Rabaud <vrabaud@willowgarage.com>
2013-04-08 00:27:23 +0200

    comply to the new DB headers

--- include/object_recognition_tabletop/household.h
+++ include/object_recognition_tabletop/household.h
@@ -37,7 +37,6 @@
 #define DB_SQL_HOUSEHOLD_H_
 
 #include <object_recognition_core/db/db.h>
-#include <object_recognition_core/db/db_base.h>
 #include <object_recognition_core/common/types.h>
 #include <household_objects_database/objects_database.h>
 
